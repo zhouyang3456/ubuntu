1d25d5f9dcae262785ab2f516f867de24fcd49bd
zhouyang3456 <zhouyang3456@outlook.com>
2023-07-26 15:38:27 +0800

    update

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+void T_produce() {
+    while (1)
+    {
+        cout << "<>";
+        cout.flush();
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+}
 
 int main(int argc, char const *argv[])
 {
     using namespace std;
-    cout << "hello world" << endl;
-    cout << "nice to see you my friend";    
-    int i = 0;
+    thread t1(T_produce);
+    t1.join();    
     return 0;
 }
